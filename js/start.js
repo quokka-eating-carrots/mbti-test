57394462415c207d6786b82196d7e3858dab62df
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-19 13:48:03 +0900

    feat: Create first qna page

--- js/start.js
+++ js/start.js
@@ -1,6 +1,31 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 
+function addAnswer (answerText, qIdx) {
+  const a = document.querySelector('.answerBox');
+  const answerBtn = document.createElement('button');
+  answerBtn.className = 'answerList'
+  a.appendChild(answerBtn);
+  answerBtn.innerHTML = answerText;
+
+  answerBtn.addEventListener("click", function () {
+    const children = document.querySelectorAll('.answerList');
+    for (let i = 0; i < children.length; i += 1) {
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+    goNext(qIdx += 1);
+  });
+}
+
+function goNext (qIdx) {
+  const q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -10,6 +35,8 @@ function begin() {
     setTimeout(() => {
       main.style.display = "none";
       qna.style.display = "block";
-    }, 450)
-  }, 450)
+    }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
 }
